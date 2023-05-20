166dff76846ceeb220afecf4d140b895aadad155
Alexandra Bunch <alexandrabunch@icloud.com>
2023-05-20 07:39:32 -0400

    Update

--- plant_disease_project/plant_disease_app/crophealth/templates/image.py
+++ plant_disease_project/plant_disease_app/crophealth/templates/image.py
@@ -3,7 +3,7 @@ import os
 import requests
 from googlesearch import search
 
-app = Flask('testapp')
+app = Flask(__name__)
 snippet = ""
 disease_name = ""
 
@@ -16,18 +16,7 @@ def process():
 #RUN SOFTWARE HERE
 #VOID IF NO DISEASE IS FOUND
 disease_name="apple black rot"
-'''
-def search_plant_disease(disease_name):
-    api_key = 'AIzaSyDHJ_-UNx41ZcdnzHos6l6Zb7n1cd0gX5A'  
-    search_engine_id = 'https://cse.google.com/cse.js?cx=711a8a876e84d4ea4' 
-    query = f'{disease_name} plant disease care tips'
-    search_url = f'https://www.googleapis.com/customsearch/v1?key={api_key}&cx={search_engine_id}&q={query}'
-    response = requests.get(search_url)
-    search_results = response.json()
-    if 'items' in search_results:
-        top_result = search_results['items'][0]  
-        snippet = top_result['snippet']
-'''
+
 def search_plant_disease(disease):
     query = f"{disease} plant disease care tips"
     num_results = 5  
@@ -37,18 +26,15 @@ def search_plant_disease(disease):
         results.append(result)
     return results
 
-index = open("results.html").read().format(disease=disease_name, 
-                                         links=search_plant_disease(disease_name))
-'''
-html_file = "results.html"
-with open(html_file, "a") as file:
-    file.write("<h1>Search Results for '{disease_name}'</h1>")
-    for data in search_plant_disease(disease_name):
-         file.write(f"<p>{data}</p>")
+@app.route('/process', methods=['POST'])
+def process():
+    disease_name = request.form['disease_name']
+    links = search_plant_disease(disease_name)
+    return render_template('results.html', disease=disease_name, links=links)
 
+@app.route('/')
 def index():
-    return render_template('results.html', disease=disease_name, links=search_plant_disease(disease_name))
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
-'''
